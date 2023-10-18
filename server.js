445e10dfab9b1ca3a902c57f3e3d962753ca5c37
sam johnson <johnson.sam26.sj@gmail.com>
2023-10-18 10:52:04 -0400

    nfl api uninstall

--- server.js
+++
@@ -1,4 +0,0 @@
-const {eapi} = require('espn-fantasy-football-api');
-const {eapiNode} = require('espn-fantasy-football-api/node');
-const {eWebdev} = require('espn-fantasy-football-api/web-dev');
-const {eNodeDev} = require('espn-fantasy-football-api/node-dev');
