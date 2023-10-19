a8bba88344a3baa98a031693b3ab054abfce7a66
Andrew <drewhis601@hotmail.com>
2023-10-19 11:48:56 -0400

    tidy up some stuff

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const exphbs = require('express-handlebars');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require('./controller');
+const sequelize = require('./config/connection.js');
 const helpers = require('./utils/helpers');
 
 const app = express();
